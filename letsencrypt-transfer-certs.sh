3dfeaf29d5ede97f6b90fc696b428af8a00d6d44
root <root@vps02.filipnet.de>
2021-10-27 06:39:23 +0200

    Check and create destination folder

--- letsencrypt-transfer-certs.sh
+++ letsencrypt-transfer-certs.sh
@@ -6,13 +6,15 @@
 # License       : BSD 3-Clause "New" or "Revised" License
 # ======================================================================================
 
-EXTRAOPT="--ipv4 --numeric-ids"
+EXTRAOPT="--ipv4 --numeric-ids --recursive"
 
 if [ -n "$1" ] ; then
 	SRC_FQDN="$1"
         SRC_FOLDER="$2"
 	DST_FOLDER="$3"
         SSH_CERT="$4"
+	echo "Create destination folder <destination folder>"
+	mkdir -p $3
 	echo "Start: $0 $1 $2 $3 $4"
 else
 	echo "Error: Usage $0 <source fqdn> <source folder> <destination folder> <ssh certificate>"
